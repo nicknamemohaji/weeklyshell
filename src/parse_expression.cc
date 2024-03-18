0b3cabb79cf37c8b4adc690741124837fde31f2f
iphuck22 <eldeshue@naver.com>
2024-03-18 15:08:50 +0900

    implement terminating condition.

--- src/parse_expression.cc
+++ src/parse_expression.cc
@@ -10,6 +10,8 @@ ast_node *parse_expression(parser *self, int precede)
 	if (self->cur_idx == self->ptoken_stream->size())
 		return ast_syntax_error(NULL);
 	left_exp = parse_prefix(self);
+	if (self->pcur_token->type == END)
+		return (left_exp);
 	while (left_exp != NULL && self->cur_idx < self->ptoken_stream->size() - 2 && precede < get_precedence(self->pnext_token->type))
 	{
 		next_node_type = get_infix_node_type(self->pnext_token->type);
