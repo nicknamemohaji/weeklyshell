d494858b8ed3d44a32df9ed2313f8e3bc1646e77
iphuck22 <eldeshue@naver.com>
2024-03-18 15:11:36 +0900

    fix error 	add condition to identify precedence of PIPE token.

--- src/get_precedence.cc
+++ src/get_precedence.cc
@@ -5,7 +5,7 @@ int get_precedence(int token_type)
 {
 	if ((token_type == OPRT_AND) || (token_type == OPRT_OR))
 		return (LOGIC);
-	else if ((token_type == RDICT_READ) || (token_type == RDICT_WRITE) || (token_type == RDICT_APPEND) || (token_type == RDICT_HEREDOC))
+	else if ((token_type == RDICT_READ) || (token_type == RDICT_WRITE) || (token_type == RDICT_APPEND) || (token_type == RDICT_HEREDOC) || (token_type == PIPE))
 		return (PIPE_AND_RDICT);
 	else
 		return (NO_MATCH);
