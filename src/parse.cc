a3eff28b80ea1f3a3794c971c96efdfe8e11bc9a
iphuck22 <eldeshue@naver.com>
2024-03-20 21:57:18 +0900

    fix error 	fix error due to using raw pointer with vector.

--- src/parse.cc
+++ src/parse.cc
@@ -15,11 +15,11 @@ ast_node *parse(std::vector<token> *ptoken_stream)
 	token end_token;
 
 	// create parser
-	p = new_parser(ptoken_stream);
 	move_redirection_token(ptoken_stream);
 	end_token.type = END;
 	ptoken_stream->push_back(end_token);
-	tree = parse_expression(p, LOWEST);
+	p = new_parser(ptoken_stream);
+	tree = parse_expression(p, P_LOWEST);
 	// parse expression
 	delete_parser(p);
 	return tree;
