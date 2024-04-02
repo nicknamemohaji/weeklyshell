42452f91eb48a1c0c3f55a237a576029b69ecef3
iphuck22 <eldeshue@naver.com>
2024-04-02 16:11:46 +0900

    fix error 	change shape of ast by modifying precedence comparing mechanism.

--- src/parse_expression.c
+++ src/parse_expression.c
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 14:07:19 by dogwak            #+#    #+#             */
-/*   Updated: 2024/03/29 19:11:02 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/04/02 16:05:21 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_ast_node	*parse_expression(t_parser *self, int precede)
 	left_exp = parse_prefix(self);
 	while (self->pcur_token->type != END && left_exp != NULL
 		&& (size_t)self->cur_idx < self->ptoken_stream->size - 2
-		&& precede < get_precedence(self->pnext_token->type))
+		&& precede <= get_precedence(self->pnext_token->type))
 	{
 		next_node_type = get_infix_node_type(self->pnext_token->type);
 		if (next_node_type == NODE_ERROR)
