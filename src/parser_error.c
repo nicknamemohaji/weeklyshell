fa70743e67f1f0bd0422e44a95f70f7fbdc36551
iphuck22 <eldeshue@naver.com>
2024-04-08 22:27:13 +0900

    fix error 	fix error at wrong fd at printing error.

--- src/parser_error.c
+++ src/parser_error.c
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 19:45:00 by dogwak            #+#    #+#             */
-/*   Updated: 2024/03/28 19:45:03 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/04/08 22:22:21 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 t_ast_node	*ast_syntax_error(t_ast_node *tree)
 {
-	write(1, "Syntax Error.\n", 14);
+	write(2, "Syntax Error.\n", 14);
 	delete_ast_node(tree);
 	return (NULL);
 }
 
 t_ast_node	*ast_malloc_error(t_ast_node *tree)
 {
-	write(1, "Memory Allocation Failure.\n", 27);
+	write(2, "Memory Allocation Failure.\n", 27);
 	delete_ast_node(tree);
 	return (NULL);
 }
